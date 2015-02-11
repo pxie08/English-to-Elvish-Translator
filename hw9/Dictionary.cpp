f7e4b8927de6ea48bc213499dc3f55bcf7aa21e8
pxie08 <pxie08@gmail.com>
2015-02-10 23:06:40 -0800

    Initial Commit
    Feb 10, 2015

---
+++ hw9/Dictionary.cpp
@@ -0,0 +1,8 @@
+/***************************************************
+** Dictionary.cpp
+** Definitions of the Dictionary
+** class member functions.
+** Patrick Xie, 03/12/10
+****************************************************/
+
+
---
+++ hw9/Dictionary.h
@@ -0,0 +1,31 @@
+/********************************************************************
+** Dictionary.h
+** Declaration for the Dictionary Class.
+** Part of hw9
+** Patrick Xie, 03/12/10
+**********************************************************************/
+
+#include <string>
+#include <vector>
+
+#ifndef CARDS_H
+#define CARDS_H
+
+using namespace std;
+
+class Dictionary {
+
+public:
+
+	Dictionary();
+
+
+
+	void translate2english();
+
+private:
+
+	
+
+};
+#endif
